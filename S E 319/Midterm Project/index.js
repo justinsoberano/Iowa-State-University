b32aaf700419d6bdfc8dc6362011454f7ed8feb3
Justin Soberano <jsoberano2002@gmail.com>
2023-03-10 20:05:02 -0600

    replaced files with updated ones

--- S E 319/Midterm Project/index.js
+++ S E 319/Midterm Project/index.js
@@ -4,6 +4,7 @@ fetch("data.json")
 
 // Universal data grabber and append for all block images.
 function dataToHtml(data) {
+
     for(let i = 0; i < data.blocks.length; i++) {
         let blockID = "block" + i;
         var block = document.getElementById(blockID);
@@ -12,9 +13,26 @@ function dataToHtml(data) {
         let blockImage = data.blocks[i].image;
         append(blockImage, block);
     }
+
+    for(let i = 0; i < data.blocks.length; i++) {
+        let textID = "blockText" + i;
+        var block = document.getElementById(textID);
+        if(block == null) {continue;}
+        let blockText = data.blocks[i].blockName;
+        appendText(blockText, block);
+    }
 }
+
 function append(picture, id) {
     let image = document.createElement("div");
     image.innerHTML = "<img class=\"card-img-top\" src=\"" + picture + "\" alt=\"Minecraft Block\" />";
     id.appendChild(image);
 }
+
+function appendText(text, id) {
+    let textBlock = document.createElement("div");
+    textBlock.innerHTML = "<h5 style=\"font-family: 'minecraftbold';\">" + text + "</h5>";
+    id.appendChild(textBlock);
+}
+
+
